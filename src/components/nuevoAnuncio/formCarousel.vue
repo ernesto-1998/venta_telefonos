e6b42486eeb404f8ea781067bf4ddc6c56f471ce
ernesto-1998 <ernestoalonso1998@gmail.com>
2021-11-13 15:56:48 -0600

    Modificando carousel

--- src/components/nuevoAnuncio/formCarousel.vue
+++ src/components/nuevoAnuncio/formCarousel.vue
@@ -1,85 +1,97 @@
 <template>
   <div class="col">
-    <div class="row">
-      <!-- COLUMNA 1 (BOTONES) -->
+    <div class="row mt-3">
 
-      <div class="col d-flex">
-        <a @click="cargarImagen()">
-          <i
-            class="bi bi-plus-circle ms-5"
-            style="font-size: 3rem; color: black"
-          ></i>
-        </a>
-      </div>
+        <!-- COLUMNA 1 (CAROUSEL) -->
 
-      <!-- COLUMNA 2 (REFERENCIAS DE IMAGENES) -->
-
-      <div class="col"></div>
-
-      <!-- COLUMNA 3 (CAROUSEL) -->
-
-      <div class="col">
-        <div class="row">
-          <div
-            v-if="imagenes.lenght !== 0"
-            id="carouselExampleInterval"
-            class="carousel slide"
-            
-            data-bs-ride="carousel"
-          >
-            <div class="carousel-inner">
-              <div
-                v-for="(img, index) in imagenes"
-                :key="index"
-                class="carousel-item"
-                :class="{ active: index == 0 }"
-                data-bs-interval="10000"
-              >
-                <img
-                  :src="img"
-                  
-                  height="300"
-                  class="d-block"
-                  alt=""
-                />
-              </div>
+        <div class="col-12">
+            <div class="row">
+            <div
+                v-if="imagenes.lenght !== 0"
+                id="carouselExampleInterval"
+                class="carousel slide"
+                
+                data-bs-ride="carousel"
+            >
+                <div class="carousel-inner">
+                <div
+                    v-for="(img, index) in imagenes"
+                    :key="index"
+                    class="carousel-item"
+                    :class="{ active: index == 0 }"
+                    data-bs-interval="10000"
+                >
+                    <img
+                    :src="img"
+                    
+                    height="300"
+                    class="d-block"
+                    alt=""
+                    />
+                </div>
+                </div>
+                <button
+                class="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev"
+                >
+                <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                class="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next"
+                >
+                <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+                </button>
             </div>
-            <button
-              class="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleInterval"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-              class="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleInterval"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
+            </div>
+
+            <div class="row d-flex">
+
+            </div>
         </div>
 
-        <div class="row d-flex">
-          <b-form-file
-            v-model="imagenP"
-            placeholder="Choose a file or drop it here..."
-            drop-placeholder="Drop file here..."
-            accept="image/*"
-          ></b-form-file>
+        <!-- COLUMNA 2 (BOTONES) -->
+
+        <div class="col-6">
+            <div class="row d-flex justify-content-start">
+                <div class="col">
+                    <a @click="cargarImagen()">
+                    <i
+                        class="bi bi-plus-circle"
+                        style="font-size: 3rem; color: black"
+                    ></i>
+                    </a>                    
+                </div>
+                <div class="col">
+                    <b-form-file
+                        v-model="imagenP"              
+                        accept="image/*"
+                    ></b-form-file>            
+                </div>
+            </div>
+
+           
         </div>
-      </div>
+
+        <!-- COLUMNA 2 (REFERENCIAS DE IMAGENES) -->
+
+        <div class="col-6">
+            <h1>Hola mundo</h1>
+        </div>
+
+
     </div>
   </div>
 </template>
