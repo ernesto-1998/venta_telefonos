3b0b82697bfc92b2c6a4d6741b4739cf82eec267
ernesto-1998 <ernestoalonso1998@gmail.com>
2021-11-16 12:52:26 -0600

    avance de cards con datos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,8 +22,10 @@
 
         <!-- FILA DE CARDS -->
 
-          <div class="row">
-            <card :info="anuncios"/>
+          <div class="row m-2">
+            <div class="col col-md-3 col-xs-12 col-sm-6" v-for="(value, index) in anuncios" :key="index">
+              <card :info="value"/>
+            </div>
           </div>
         </div>
 
@@ -56,17 +58,19 @@ export default {
     };
   },
   methods:{
-    async traerDatos(){
+    async traerAnuncios(){
       try {
         this.anuncios = []
-        const data = await db.collection('anuncios').get()
+        await db.collection('anuncios').get()
+        .then((data) => {
         data.forEach(documentos => {
           let anuncio = {
-            id: documentos.data().id,
+            id: documentos.id,
             nombre: documentos.data().nombre,
             precio: documentos.data().precio,
             telefonoContacto: documentos.data().telefonoContacto,
             titulo: documentos.data().titulo,
+            descripcion: documentos.data().descripcion,
             telefono: {
               estado: documentos.data().telefono.estado,
               marca: documentos.data().telefono.marca,
@@ -79,17 +83,18 @@ export default {
           }
           this.anuncios.push(anuncio);
           console.log(this.anuncios.id)
-        });
-        for(let anuncio of this.anuncios){
-          anuncio.foto = await st.ref().child(anuncio.id + "/1").getDownloadURL();
+        });          
+        })
+        for(let anuncio1 of this.anuncios){
+          anuncio1.foto = await st.ref().child(anuncio1.id.toString() + "/1").getDownloadURL();
         }        
       } catch (error) {
         console.log(error)
       }
     }
   },
-  mounted(){
-    this.traerDatos()
+  async created(){
+    await this.traerAnuncios()
   }
 }
 </script>
