5816c505d79094269d312b955525df18290ddb09
ernesto-1998 <ernestoalonso1998@gmail.com>
2021-11-17 01:10:58 -0600

    mejorando las card

--- src/components/home/card.vue
+++ src/components/home/card.vue
@@ -2,11 +2,12 @@
     <div class="card" style="height: 100%;">
       <img :src="anuncio.foto" class="card-img-top" height="170px" alt="..." />
       <div class="card-body text-center border border-2 border-dark">
-        <div>
-            <h4><b>${{anuncio.precio}}</b></h4>
+        <div class="row">
+            <h4 style= "color: black;"><b>${{anuncio.precio}}</b></h4>
+            <h5 style= "color: black;"><b>{{anuncio.telefono.estado}}</b></h5>
             {{anuncio.telefono.marca}} {{anuncio.telefono.modelo}}, Pantalla de {{anuncio.telefono.pantalla}} pulg. {{anuncio.telefono.ram}} GB de RAM, {{anuncio.telefono.rom}} GB de almacenamiento interno 
         </div>
-        <h5><b>{{anuncio.telefono.estado}}</b></h5>
+
       </div>
     </div>
 </template>
