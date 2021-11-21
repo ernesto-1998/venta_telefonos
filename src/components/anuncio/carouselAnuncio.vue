85e67a431c19d20a0035e5ccd4bcf232a5c8898e
ernesto-1998 <ernestoalonso1998@gmail.com>
2021-11-21 01:12:26 -0600

    preparando para anuncio

---
+++ src/components/anuncio/carouselAnuncio.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div
+      id="carouselExampleControls"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div
+          v-for="(img, index) in imagenes"
+          :key="index"
+          class="carousel-item"
+          :class="{ active: index == 0 }"
+        >
+          <img :src="img" class="d-block w-100" height="200px" alt="..." />
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleControls"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "carouselAnuncio",
+  props: {
+    imagenes: Array,
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/views/anuncio.vue
+++ src/views/anuncio.vue
@@ -1,11 +1,55 @@
-<template lang="">
-    <div>
-        
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+
+            </div>
+            <div class="col">
+                
+            </div>
+        </div> 
     </div>
 </template>
 <script>
+import carouselAnuncio from '../components/anuncio/carouselAnuncio.vue'
+
 export default {
-    name: 'anuncio'
+    name: 'anuncio',
+    components:{
+        carouselAnuncio
+    },
+    data(){
+        return{
+            imagenes: [],
+            anuncio: {
+                id: "",
+                nombre: "",
+                precio: "",
+                telefonoContacto: "",
+                titulo: "",
+                descripcion: "",
+                fecha: "",
+                foto: "",
+                telefono: {
+                    estado: "",
+                    marca: "",
+                    sistema: "",
+                    modelo: "",
+                    pantalla: "",
+                    rom: "",
+                    ram: "",
+                }
+            }
+        };
+    }
 }
 </script>
 <style lang="">
